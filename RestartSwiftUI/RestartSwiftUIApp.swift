2cfb360784ee070f4656668bc8925d952afd6fdc
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-10-31 14:59:13 +0300

    Initial Commit

---
+++ RestartSwiftUI/RestartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartSwiftUIApp.swift
+//  RestartSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
