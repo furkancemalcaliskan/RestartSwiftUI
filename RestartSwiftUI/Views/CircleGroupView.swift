0a5cc54372fc7648f42599242fcb517bf6daaac3
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-10-31 16:17:07 +0300

    Finished HomeView

---
+++ RestartSwiftUI/Views/CircleGroupView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleGroupView.swift
+//  RestartSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 31.10.2022.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    @State var ShapeColor : Color
+    @State var ShapeOpacity : Double
+    
+    var body: some View {
+        ZStack{
+          Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
