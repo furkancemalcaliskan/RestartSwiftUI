8e922723e31ae89eb36812cae247b1894f59a095
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-10-31 20:45:44 +0300

    Added Complex Gestures to OnboardingView

--- RestartSwiftUI/Screens/OnboardingView.swift
+++ RestartSwiftUI/Screens/OnboardingView.swift
@@ -11,6 +11,9 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive : Bool = true
     
+    @State private var buttonWidth : Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset : CGFloat = 0
+    
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -75,7 +78,7 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -94,14 +97,28 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                     .frame(width: 80, height: 80, alignment: .center)
-                    .onTapGesture {
-                        isOnboardingViewActive = false
-                    }
+                    .offset(x: buttonOffset)
+                    .gesture(
+                    DragGesture()
+                        .onChanged({ gesture in
+                            if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                buttonOffset = gesture.translation.width
+                            }
+                        })
+                        .onEnded({ _ in
+                            if buttonOffset > buttonWidth / 2 {
+                                buttonOffset = buttonWidth - 80
+                                isOnboardingViewActive = false
+                            } else {
+                                buttonOffset = 0
+                            }
+                        })
+                    )//: Gesture
                         
                         Spacer()
                     }//: HSTACK
                 }//: Footer
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
                 }//: VSTACK
             }//: ZSTACK
